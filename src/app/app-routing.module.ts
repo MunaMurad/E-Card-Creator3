1127056ce03dcb0eaa77721aede89574cf0e2113
Merna-GHub <mrn.Ghub@gmail.com>
2021-11-12 15:30:45 +0300

    Fixes to routingModul

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1419,3 +1419,11 @@ const routes: Routes = [
   },
 
 ];
+
+@NgModule({
+  imports: [
+    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })
+  ],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
