edbc6efbbe769757c9497bc1747d4437fd9083b4
Amal Saleh <amal.saleh.mu@gmail.com>
2021-12-01 19:13:01 +0300

    update

--- src/app/occasions/invitation/invitation.page.ts
+++ src/app/occasions/invitation/invitation.page.ts
@@ -1,5 +1,10 @@
 import { Component, OnInit } from '@angular/core';
-
+import { AngularFirestoreCollection } from '@angular/fire/firestore';
+import { InvitationDetails } from 'src/app/congratspages/image-editor/InvitationDetails';
+import { UserService } from "../../services/user.service";
+import { AngularFirestore,AngularFirestoreDocument} from '@angular/fire/firestore';
+import * as firebase from 'firebase';
+import { Storage } from '@ionic/storage';
 @Component({
   selector: 'app-invitation',
   templateUrl: './invitation.page.html',
@@ -8,8 +13,36 @@ import { Component, OnInit } from '@angular/core';
 export class InvitationPage implements OnInit {
 
   selectedItem: string = "one";
+ 
+  private invitationsCollection: AngularFirestoreCollection<any>;
+  
+  userId:any;
+  invitations:InvitationDetails[];
+  constructor(public userService:UserService,private storage:Storage, private firestore: AngularFirestore) {
 
-  constructor() { }
+    this.invitationsCollection = this.firestore.collection<any>('Invitations');
+    this.storage.get('userId').then((val)=>{
+      console.log("this.userId = val inside",val)
+      this.userId = val;
+    });
+    console.log('this.storage.get(userId) outside',this.userId);
+   }
+
+async getInvitationCounter(){
+  
+  console.log('start getInvitationCounter');
+  var data = await this.invitationsCollection.ref.where('UserId', '==', this.userId).get().then(
+    function (querySnapshot){
+      querySnapshot.forEach((res)=>
+      {
+        var result = res.data() as InvitationDetails;
+      console.log('start this.invitation.push', result);
+      this.invitation.push(res.data() as InvitationDetails);
+      })
+  });        
+  console.log('start invitationArray', this.invitations);
+  return this.invitations;
+}
 
   ngOnInit() {
   }
